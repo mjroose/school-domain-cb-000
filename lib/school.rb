5072f0d75a7fecc8981554d0597108926a3842c3
Michael Roose <mjroose@gmail.com>
2018-08-10 00:33:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, :grade)
+  def add_student(student_name, grade)
     if self.roster[:grade]
       self.roster[:grade] << student_name
     else
